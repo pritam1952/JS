4e50dbf9d5a41112c97c5ac9fb7ab14a70b09a96
Pritam Kumar <147640305+pritam1952@users.noreply.github.com>
2024-02-03 02:15:41 +0000

    datatypes

---
+++ 01-basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version.
+// alert(3+3)
+//we are using node js not browser.
+let name="Pritam"
+let age=18
+let isloggedIn=false//boolean
+
+//data types
+//numbers->> range 2to the power of 53
+// bigint 
+//string=>""
+//boolean => true/false
+//null => standlone value//place of empty space
+//undefined => whenever we defined variales and assign any value then it's called undefined.
+// symbol =>unique
+// object
+console.log(typeof undefined);// undefined
+console.log(typeof null );//object
